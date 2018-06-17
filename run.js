5ee3c127f4fa8278288e4f48104ae20239afb439
mennyp <mpinhaso@gmail.com>
2018-06-17 15:32:12 +0300

    finished drive id

--- run.js
+++ run.js
@@ -1,9 +1,34 @@
 var express = require('express');
-var app = express();
+var request = require('request');
+var Cookie = require('request-cookies').Cookie;
 
-app.get('/', function (req, res){
-    res.send('Hello there, world!\n');
+var _request = request.defaults({jar: true});
+var app = express();
+var driveUrl = 'https://docs.google.com/uc?export=download';
+
+app.get('/driveid/:driveid', function (req, res){
+    //res.send(req.params)
+    var id = req.params['driveid'];
+    var options = {
+        method: 'HEAD',
+        uri: driveUrl,
+        qs: {
+            id: id
+        }
+    };
+    _request.get(options, function(err, response, body) {
+        var rawcookies = response.headers['set-cookie'];
+        var cookie = new Cookie(rawcookies[0]);
+        console.log(cookie.value);
+        //1fuUp1_K1SfUwponbHKJYJXxf_6vU4RoM
+        options.qs.confirm = cookie.value;
+        _request.head(options, function(err, response, body) {
+            console.log(response.request.uri.href);
+            res.redirect(response.request.uri.href);
+          });
+    });
 });
+
 
 var port = 9000;
 app.listen(port);
