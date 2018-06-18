0f7e6f8890ddbb2ab8f0f71d7f1f44d7f53a1233
mennyp <mpinhaso@gmail.com>
2018-06-18 10:59:40 +0300

    add log

--- run.js
+++ run.js
@@ -1,13 +1,22 @@
 var express = require('express');
 var request = require('request');
 var Cookie = require('request-cookies').Cookie;
+var logger = require('morgan');
 
 var _request = request.defaults({jar: true});
 var app = express();
 var driveUrl = 'https://docs.google.com/uc?export=download';
 
+
+logger.format('mydate', function() {
+    var df = require('log-timestamp');
+    return df(new Date(), 'HH:MM:ss.l');
+});
+app.use(logger('[:mydate] :method :url :status :res[content-length] - :remote-addr - :response-time ms'));
+
 app.get('/driveid/:driveid', function (req, res){
     //res.send(req.params)
+    require('log-timestamp');
     var ip = (req.headers['x-forwarded-for'] ||
      req.connection.remoteAddress ||
      req.socket.remoteAddress ||
@@ -26,11 +35,11 @@ app.get('/driveid/:driveid', function (req, res){
         var rawcookies = response.headers['set-cookie'];
         if (rawcookies){
             var cookie = new Cookie(rawcookies[0]);
-            console.log(cookie.value);
+            //console.log(cookie.value);
             //1fuUp1_K1SfUwponbHKJYJXxf_6vU4RoM
             options.qs.confirm = cookie.value;
             _request.head(options, function(err, response, body) {
-                console.log(response.request.uri.href);
+                console.log('redirecting...');
                 res.redirect(response.request.uri.href);
             });
         }
